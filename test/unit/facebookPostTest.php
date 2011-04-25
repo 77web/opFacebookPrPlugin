900e9cede0417cb89c7c1c553d4b2be432e6f75e
77web <info@77-web.com>
2011-04-25 17:25:39 +0900

    remove unnecessary tests, remove japanese texts in unit test, update README

--- test/unit/facebookPostTest.php
+++ test/unit/facebookPostTest.php
@@ -4,7 +4,7 @@ include(dirname(__FILE__).'/../bootstrap/unit.php');
 $configuration = ProjectConfiguration::getApplicationConfiguration('pc_frontend', 'test', true);
 new sfDatabaseManager($configuration);
 
-$ret = opFacebookPrPluginClient::postWall('テスト投稿', 'http://www.77-web.com/');
+$ret = opFacebookPrPluginClient::postWall('this is test', 'http://sns.openpne.jp/');
 
 $t = new lime_test(1, new lime_output_color());
 $t->ok($ret);
